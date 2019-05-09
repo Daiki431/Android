46c02edd325c33d912bf9548a14218fcfdce8814
Daiki431 <dai.take431@gmail.com>
2019-05-09 19:09:22 +0900

    complete Kadai1

--- Kadai1_3B11KS/app/src/main/java/com/websarva/wings/android/kadai1_3b11ks/MainActivity.java
+++ Kadai1_3B11KS/app/src/main/java/com/websarva/wings/android/kadai1_3b11ks/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView dateText = (TextView)findViewById(R.id.date_id);
-　　　　Date date = new Date();
-　　　　SimpleDateFormat sdf = new SimpleDateFormat("yyyy'年'MM'月'dd'日'　kk'時'mm'分'ss'秒'");
-　　　　dateText.setText(sdf.format(date));
+        TextView dateText = findViewById(R.id.dateTv);
+        TextView timeText = findViewById((R.id.timeTv));
+        Date d = new Date();
+        SimpleDateFormat sTime = new SimpleDateFormat("HH時mm分ss秒", Locale.JAPANESE);
+        SimpleDateFormat sDate = new SimpleDateFormat("yyyy年MM月dd日(E)",Locale.JAPANESE);
+        timeText.setText("現在の時刻："+sTime.format(d));
+        dateText.setText("今日の日付："+sDate.format(d));
     }
+
+
+
 }
