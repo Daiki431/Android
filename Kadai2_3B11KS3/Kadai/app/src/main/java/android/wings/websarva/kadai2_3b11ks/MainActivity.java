53d5930fa1ece0e423cc499a521666228c2a454f
Daiki431 <dai.take431@gmail.com>
2019-05-23 14:12:31 +0900

    remove_Kadais

---
+++ Kadai2_3B11KS3/Kadai/app/src/main/java/android/wings/websarva/kadai2_3b11ks/MainActivity.java
@@ -0,0 +1,13 @@
+package android.wings.websarva.kadai2_3b11ks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
